da2d9578d9c5b94ccc94d387bfb0d9db2c1819b0
Alejandro Guerrero <alejo.jesus.magne@gmail.com>
2022-08-14 20:47:50 -0400

    Movies Details

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:movies/screens/screens.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movies',
+      initialRoute: 'home',
+      routes: {
+        'home': ( _ ) => const HomeScreen(),
+        'details': ( _ ) => const DetailsScreen(),
+      },
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: const AppBarTheme(
+          color: Colors.orange
+        )
+      ),
+    );
+  }
+}
